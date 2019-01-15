d0fff4980ca86e40d0893e799633656a5bde4398
Ahren09 <1099052927@qq.com>
2019-01-15 08:29:21 -0800

    1

---
+++ Projects/Project1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <utility>
+#include <cstdlib>
+
+#include "History.h"
+#include "Arena.h"
+#include "Zombie.h"
+#include "Player.h"
+#include "Game.h"
+#include "globals.h"
+
+using namespace std;
+
+int main()
+{
+    // Create a game
+    // Use this instead to create a mini-game:   Game g(3, 4, 2);
+    Game g(7, 8, 25);
+    
+    // Play the game
+    g.play();
+}
